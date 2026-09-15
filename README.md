aad22fce2cc45f2d12ba8749f98b5ea981e8738a
Owen Kuo <owennkuo@gmail.com>
2026-09-15 19:12:59 +0000

    general code cleanup and README clarifications

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # Scouting Schedules
-A simple tool for creating minimal scouting schedules using the TBA API.
+A simple tool for creating minimal scouting schedules using the TBA API for use in the FIRST Robotics Competition.
+
+For each match with the target team:
+Returns the latest, previous match of every other team in the current match.
 
 ## Usage
 All values you need are at the top of the python file, including the event key, the team key, and the TBA API key. Run the program and it will generate a minimal schedule for you.
 
+Set the required values:
+- Your team key
+- Event key
+- TBA API key
+
 ### Running the script
-- Activate the Virtual Environment: `source .venv/bin/activate`
+- Create a Virtual Environment: `python3 -m venv .venv`
+- Activate the Virtual Environment: `source .venv/bin/activate` or `.\.venv\Scripts\Activate.ps1`
+- Install `requests`: `python3 -m pip install requests`
 - Run the python file: `python3 main.py`
 
 Output is saved to a file in the format `{event_key}_schedule.csv`
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # DEFINITIONS
-event_key = "2026cacac"
+event_key = "2026cancmp"
 team_key = "frc5507"
 api_key = "1pASptoMl6xXHptBnpRfEG3KFU6ghqdU9pzDNs3WWaD9hXsL6LDW22imF99SZlFA"
 
@@ -7,29 +7,33 @@ import sqlite3
 import requests
 import csv
 
+#Connect to the database and clear the tables of previous data.
 conn = sqlite3.connect("data.db")
 cursor = conn.cursor()
 query = "INSERT INTO event_matches VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
 cursor.execute("DELETE FROM event_matches")
 cursor.execute("DELETE FROM scouting")
 
+#Fetch the match schedule from the TBA endpoint using the event code.
 matches_data = requests.get(f"https://www.thebluealliance.com/api/v3/event/{event_key}/matches", headers={'X-TBA-AUTH-KEY':api_key}).json()
-#print(matches_data)
 
+#Flatten the json data into an SQLite table containing only the data we need (match id, match number, team keys)
 for match in matches_data:
     match_id = match.get("key")
     red_teams = match["alliances"]["red"]["team_keys"]
     blue_teams = match["alliances"]["blue"]["team_keys"]
     cursor.execute(query, (match_id, match["comp_level"], match["match_number"], red_teams[0], red_teams[1], red_teams[2], blue_teams[0], blue_teams[1], blue_teams[2]))
 
-cursor.execute("""SELECT * FROM event_matches
+#Select all Qualifying matches with the target team
+cursor.execute(f"""SELECT * FROM event_matches
                   WHERE comp_level = 'qm'
-                  AND 'frc5507' IN (R1, R2, R3, B1, B2, B3)
+                  AND '{team_key}' IN (R1, R2, R3, B1, B2, B3)
                   ORDER BY match_number""")
 matches = cursor.fetchall()
-index = 1
+
 # For each match with the target team key, find the last match for each opponent.
 for row in matches:
+    #Repeat for each team in the given match.
     for i in range(6):
         if row[i+3] == team_key:
             continue
@@ -46,19 +50,18 @@ for row in matches:
                 """,
                 (match[0], match[2], row[i+3], row[0]),
             )
-    index += 1
 
+#Select the entire scouting table and fetch the headers/normal rows.
 cursor.execute(f"SELECT * FROM scouting")
 headers = [description[0] for description in cursor.description]
 rows = cursor.fetchall()
 
+#write the contents of the table into a new csv file.
 output_file = event_key + "_schedule.csv"
 with open(output_file, "w", newline="", encoding="utf-8") as f:
     writer = csv.writer(f)
     writer.writerow(headers)
-
     writer.writerows(rows)
-
 
 conn.commit()
 conn.close()
